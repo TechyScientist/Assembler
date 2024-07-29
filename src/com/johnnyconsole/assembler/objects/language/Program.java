0054552905913bb0622b9baa75fe7d310458635d
Johnny Console <johnny.console@hotmail.com>
2024-07-29 09:38:11 -0400

    Import GPRs to the program class

--- src/com/johnnyconsole/assembler/objects/language/Program.java
+++ src/com/johnnyconsole/assembler/objects/language/Program.java
@@ -1,4 +1,16 @@
 package com.johnnyconsole.assembler.objects.language;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static com.johnnyconsole.assembler.objects.language.Registers.*;
+
 public class Program {
+
+    private final ArrayList<String> instructions = new ArrayList<>();
+    private final HashMap<String, Variable> variables = new HashMap<>();
+    private final HashMap<String, Procedure> procedures = new HashMap<>();
+
+
+
 }
