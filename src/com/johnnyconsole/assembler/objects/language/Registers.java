4a23c1f3fa18e209ebf9475e3f24a43c78d73e4b
Johnny Console <johnny.console@hotmail.com>
2024-07-29 09:37:47 -0400

    Add get() function to Registers.java to return Register object for a given register name

--- src/com/johnnyconsole/assembler/objects/language/Registers.java
+++ src/com/johnnyconsole/assembler/objects/language/Registers.java
@@ -1,5 +1,10 @@
 package com.johnnyconsole.assembler.objects.language;
 
+import com.johnnyconsole.assembler.objects.language.error.IllegalOperationError;
+import com.johnnyconsole.assembler.objects.language.error.InvalidRegisterError;
+
+import java.util.Locale;
+
 public final class Registers {
 
     public static Register EAX = new Register("EAX");
@@ -12,6 +17,51 @@ public final class Registers {
     public static Register ESP = new Register("ESP");
     public static Register EIP = new Register("EIP");
     public static Register EFLAGS = new Register("EFLAGS");
+
+    public static Register get(String registerName) throws IllegalOperationError, InvalidRegisterError {
+        if(registerName.equalsIgnoreCase("EIP") ||
+                registerName.equalsIgnoreCase("IP") ||
+                registerName.equalsIgnoreCase("EFLAGS") ||
+                registerName.equalsIgnoreCase("FLAGS")) {
+            throw new IllegalOperationError("Illegal Register Access: " + registerName);
+        }
+        switch (registerName.toUpperCase(Locale.ROOT)) {
+            case "EAX":
+            case "AX":
+            case "AL":
+            case "AH":
+                return EAX;
+            case "EBX":
+            case "BX":
+            case "BL":
+            case "BH":
+                return EBX;
+            case "ECX":
+            case "CX":
+            case "CL":
+            case "CH":
+                return ECX;
+            case "EDX":
+            case "DX":
+            case "DL":
+            case "DH":
+                return EDX;
+            case "ESI":
+            case "SI":
+                return ESI;
+            case "EDI":
+            case "DI":
+                return EDI;
+            case "EBP":
+            case "BP":
+                return EBP;
+            case "ESP":
+            case "SP":
+                return ESP;
+            default:
+                throw new InvalidRegisterError("Invalid Register: " + registerName);
+        }
+    }
 
 
 }
