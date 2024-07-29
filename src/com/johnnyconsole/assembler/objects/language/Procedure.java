b0825f45136053f5561a497734e5290b8c00a435
Johnny Console <johnny.console@hotmail.com>
2024-07-29 09:43:04 -0400

    Add code to Procedure class

--- src/com/johnnyconsole/assembler/objects/language/Procedure.java
+++ src/com/johnnyconsole/assembler/objects/language/Procedure.java
@@ -1,4 +1,25 @@
 package com.johnnyconsole.assembler.objects.language;
 
+import java.util.ArrayList;
+
 public class Procedure {
+
+    private final String name;
+    private final ArrayList<String> instructions = new ArrayList<>();
+
+    public Procedure(String name) {
+        this.name = name;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public String[] instructions() {
+        return instructions.toArray(new String[0]);
+    }
+
+    public void addInstruction(String instruction) {
+        instructions.add(instruction);
+    }
 }
